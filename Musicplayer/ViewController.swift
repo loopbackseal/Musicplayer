c9f9745b9a16bac1ba50ef4c50ed06265f4f5529
loopbackseal <young961027@gmail.com>
2021-12-12 17:56:33 +0900

    Initial Commit

---
+++ Musicplayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musicplayer
+//
+//  Created by Young Soo Hwang on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
