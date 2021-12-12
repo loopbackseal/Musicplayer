fe38e241c37c3be69556586468c4b2751abf9f54
loopbackseal <young961027@gmail.com>
2021-12-12 20:20:54 +0900

    IBAction in Interface Builder

--- Musicplayer/ViewController.swift
+++ Musicplayer/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func touchUpPlayButton(_ sender: UIButton) {
+        print("button tapped")
+    }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
 }
 
